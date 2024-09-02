bdae34dc4b3850074cee95d68c40604fe2d25759
sepideh <sepideh0771@gmail.com>
2024-09-02 17:13:23 +0330

    Filter Tab Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,36 @@
 const courseStatus = ["All", "Active", "Completed", "Upcoming"];
-import React from "react";
+import React, { useState } from "react";
+import CourseList from "./CourseList";
 
-function Header() {
+function Header({ courses }) {
+  const [statusTab, setStatusTab] = useState("");
+  const clickHandler = (s) => {
+    console.log(s);
+    setStatusTab(s);
+  };
+  console.log(
+    courses.map((item) => item).filter((result) => result.status == statusTab)
+  );
+
   return (
     <div>
       <h1>My Courses (3)</h1>
       <div className="course-status">
         {courseStatus.map((s) => (
-          <div className="active" key={s}>
+          <div className="active" key={s} onClick={() => clickHandler(s)}>
             {s}
           </div>
         ))}
       </div>
+      {statusTab == "All" ? (
+        <CourseList courses={courses} />
+      ) : (
+        <CourseList
+          courses={courses
+            .map((item) => item)
+            .filter((result) => result.status == statusTab)}
+        />
+      )}
     </div>
   );
 }
